a3f45c2190019038d6bb615683ee8fcec47ae0db
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-10-22 19:17:22 -0500

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -1,9 +1,9 @@
-# Create an ECR Repository
+#To Create an ECR Repository
 aws ecr create-repository --repository-name t2s-web-app --region us-east-1
 
-# Install Trivy
+#To Install Jenkins on your Local Machine
+
+#To Install Trivy on your Local Machine
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew install aquasecurity/trivy/trivy
-
-trivy --version
+brew install aquasecurity/trivy/trivy #To install Trivy
+trivy --version  #To check the version
