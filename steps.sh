5896bdd79a072828408938e400614921e0ac20a5
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-06 09:29:49 -0600

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -4,6 +4,6 @@ aws ecr create-repository --repository-name t2s-web-app --region us-east-1
 # Install Trivy
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install aquasecurity/trivy/trivy
+brew install aquasecurity/trivy/trivy # Installing on MacOS. On the other systems, use "choco," "apt," "yum," etc.
 
 trivy --version
